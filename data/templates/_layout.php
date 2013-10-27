8d1628e029d592688402ca190046fcaa5f5919e8
makki_d <d.makiuchi@gmail.com>
2013-10-27 13:05:48 +0900

    bootstrap

--- data/templates/_layout.php
+++ data/templates/_layout.php
@@ -2,34 +2,54 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <title>EM-Launcher</title>
-    <link rel="stylesheet" href="<?=url('/css/common.css')?>" type="text/css">
+    <title><?=(isset($page_title))?$title.' | ':''?>EM-Launcher</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php if(mfwServerEnv::getEnv()==='local'): ?>
+    <link href="/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <?php else: ?>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <?php endif ?>
+    <link rel="stylesheet" href="<?=url('/css/customize.css')?>" type="text/css">
   </head>
   <body>
-    <div id="navbar">
-      <div id="loginuser" <?=($this->login_user)?'':'style="visibility:visible"'?>>
-        <div class="address">
-          makiuchi-d@klab.com
-        </div>
-        <ul id="usermenu">
-          <li><a href="">logout</a></li>
-          <li><a href="">logout</a></li>
-          <li><a href="">logout</a></li>
+
+    <div class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="<?=url('/mypage')?>" class="navbar-brand"><span>EM</span><span>Launcher</span></a>
+      </div>
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="<?=url('/project')?>">Projects</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">makiuchi-d@klab.com <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="<?=url('/mypage')?>">Mypage</a></li>
+              <li><a href="<?=url('/project/create')?>">New project</a></li>
+              <li><a href="<?=url('/mypage/logout')?>">Logout</a></li>
+            </ul>
+          </li>
         </ul>
       </div>
-      <h1><span class="em">EM</span><span class="launcher">Launcher</span></h1>
     </div>
+
     <?=$contents?>
   </body>
-  <script type="text/javascript">
-  document.getElementById('loginuser').onclick = function(){
-    var e = document.getElementById('usermenu');
-    if(e.style.visibility=='visible'){
-      e.style.visibility = 'hidden';
-    }
-    else{
-      e.style.visibility = 'visible';
-    }
-  };
+
+  <?php if(mfwServerEnv::getEnv()==='local'): ?>
+  <script src="/jquery/jquery.js"></script>
+  <script src="/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <?php else: ?>
+  <script src="//code.jquery.com/jquery.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <?php endif ?>
+
   </script>
 </html>
