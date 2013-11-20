6f3ed500adecd3b4f3e9c3e26af18df45c387aee
makki_d <d.makiuchi@gmail.com>
2013-11-20 22:18:00 +0900

    forget adding Config.php

---
+++ model/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+class Config
+{
+	const CONFIG_FILE = '/config/emlauncher_config.php';
+	protected static $config = null;
+
+	public static function get($key)
+	{
+		if(static::$config===null){
+			include APP_ROOT . static::CONFIG_FILE;
+			static::$config = $emlauncher_config[mfwServerEnv::getEnv()];
+		}
+		return isset(static::$config[$key])? static::$config[$key]: null;
+	}
+
+}
