98bc9d2d41e08e49310fff5c1d37c43a5414b3ef
karino-t <karino-t@klab.com>
2014-10-31 23:03:01 +0900

    space -> tab

--- mainmodules/MainActions.php
+++ mainmodules/MainActions.php
@@ -4,94 +4,94 @@ require_once APP_ROOT.'/model/Config.php';
 
 class MainActions extends mfwActions
 {
-    const TEMPLATEDIR = '/data/templates';
-    const BLOCKDIR = '/data/blocks';
+	const TEMPLATEDIR = '/data/templates';
+	const BLOCKDIR = '/data/blocks';
 
-    const SESKEY_URL_BEFORE_LOGIN = 'url_before_login';
+	const SESKEY_URL_BEFORE_LOGIN = 'url_before_login';
 
-    protected $login_user = null;
+	protected $login_user = null;
 
-    public function initialize()
-    {
-        if(($err=parent::initialize())){
-            return $err;
-        }
+	public function initialize()
+	{
+		if(($err=parent::initialize())){
+			return $err;
+		}
 
-        // いくつかのapiはAPI Key認証なのでログイン不要
-        if($this->module==='api'){
-            if(in_array($this->action,array('upload','package_list','delete', 'create_token'))){
-                return null;
-            }
-        }
+		// いくつかのapiはAPI Key認証なのでログイン不要
+		if($this->module==='api'){
+			if(in_array($this->action,array('upload','package_list','delete', 'create_token'))){
+				return null;
+			}
+		}
 
-        // package/install_plist はセッションが使えないため別途認証する.
-        if($this->module==='package' && $this->action==='install_plist'){
-            return null;
-        }
+		// package/install_plist はセッションが使えないため別途認証する.
+		if($this->module==='package' && $this->action==='install_plist'){
+			return null;
+		}
 
-        $this->login_user = User::getLoginUser();
-        if(!$this->login_user && $this->getModule()!='login'){
-            $scheme = Config::get('enable_https')?'https':null;
-            $this->saveUrlBeforeLogin($scheme);
-            return $this->redirect(mfwRequest::makeUrl('/login',$scheme));
-        }
+		$this->login_user = User::getLoginUser();
+		if(!$this->login_user && $this->getModule()!='login'){
+			$scheme = Config::get('enable_https')?'https':null;
+			$this->saveUrlBeforeLogin($scheme);
+			return $this->redirect(mfwRequest::makeUrl('/login',$scheme));
+		}
 
-        if($this->login_user){
-            apache_log('user',$this->login_user->getMail());
-        }
-        return null;
-    }
+		if($this->login_user){
+			apache_log('user',$this->login_user->getMail());
+		}
+		return null;
+	}
 
-    protected function build($params=array(),$headers=array())
-    {
-        $params['login_user'] = $this->login_user;
-        $params['module'] = $this->module;
-        $params['action'] = $this->action;
-        $params['title_prefix'] = Config::get('title_prefix');
-        return parent::build($params,$headers);
-    }
+	protected function build($params=array(),$headers=array())
+	{
+		$params['login_user'] = $this->login_user;
+		$params['module'] = $this->module;
+		$params['action'] = $this->action;
+		$params['title_prefix'] = Config::get('title_prefix');
+		return parent::build($params,$headers);
+	}
 
 
-    protected function buildErrorPage($message,$headers=array())
-    {
-        $params = array(
-            'message' => $message,
-            );
-        $this->setTemplateName('_error');
-        return $this->build($params,$headers);
-    }
+	protected function buildErrorPage($message,$headers=array())
+	{
+		$params = array(
+			'message' => $message,
+			);
+		$this->setTemplateName('_error');
+		return $this->build($params,$headers);
+	}
 
-    protected function response($status,$message=null)
-    {
-        return array(array($status),$message);
-    }
+	protected function response($status,$message=null)
+	{
+		return array(array($status),$message);
+	}
 
-    protected function saveUrlBeforeLogin()
-    {
-        if(mfwRequest::method()==='GET'){
-            $url = mfwRequest::url();
-            if(Config::get('enable_https')){
-                $url = preg_replace('/^http:/','https:',$url);
-            }
-            mfwSession::set(self::SESKEY_URL_BEFORE_LOGIN,$url);
-        }
-        else{
-            mfwSession::clear(self::SESKEY_URL_BEFORE_LOGIN);
-        }
-    }
+	protected function saveUrlBeforeLogin()
+	{
+		if(mfwRequest::method()==='GET'){
+			$url = mfwRequest::url();
+			if(Config::get('enable_https')){
+				$url = preg_replace('/^http:/','https:',$url);
+			}
+			mfwSession::set(self::SESKEY_URL_BEFORE_LOGIN,$url);
+		}
+		else{
+			mfwSession::clear(self::SESKEY_URL_BEFORE_LOGIN);
+		}
+	}
 
-    protected function redirectUrlBeforeLogin()
-    {
-        $url = mfwSession::get(self::SESKEY_URL_BEFORE_LOGIN);
-        if(!$url){
-            $url = '/';
-        }
-        return $this->redirect($url);
-    }
+	protected function redirectUrlBeforeLogin()
+	{
+		$url = mfwSession::get(self::SESKEY_URL_BEFORE_LOGIN);
+		if(!$url){
+			$url = '/';
+		}
+		return $this->redirect($url);
+	}
 
-    protected function clearUrlBeforelogin()
-    {
-        mfwSession::clear(self::SESKEY_URL_BEFORE_LOGIN);
-    }
+	protected function clearUrlBeforelogin()
+	{
+		mfwSession::clear(self::SESKEY_URL_BEFORE_LOGIN);
+	}
 
 }
