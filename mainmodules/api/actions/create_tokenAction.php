258499bb530ab9e05e6fdc3a89328aa503b272dc
karino-t <karino-t@klab.com>
2014-10-31 18:02:14 +0900

    add docs

--- mainmodules/api/actions/create_tokenAction.php
+++ mainmodules/api/actions/create_tokenAction.php
@@ -3,38 +3,63 @@ require_once __DIR__.'/actions.php';
 require_once APP_ROOT.'/model/Application.php';
 require_once APP_ROOT.'/model/Package.php';
 
-class package_listAction extends apiActions
+class create_install_tokenAction extends apiActions
 {
-	public function executePackage_list()
-	{
-		try{
-			$api_key = mfwRequest::param('api_key');
+    public function executeCreate_install()
+    {
+        try{
+            $api_key = mfwRequest::param('api_key');
+            $pkg_id  = mfwRequest::param('id');
+            $mail    = mfwRequest::param('mail');
 
-			$app = ApplicationDb::selectByApiKey($api_key);
-			if(!$app){
-				return $this->jsonResponse(
-					self::HTTP_400_BADREQUEST,
-					array('error'=>'Invalid api_key'));
-			}
+            $app = ApplicationDb::selectByApiKey($api_key);
+            if(!$app){
+                return $this->jsonResponse(
+                    self::HTTP_400_BADREQUEST,
+                    array('error'=>'Invalid api_key'));
+            }
 
-			$pkgs = PackageDb::selectByAppId($app->getId());
+            # id check
+            $pkg = PackageDb::retrieveByPK($pkg_id);
+            if(!$pkg || $app->getId()!==$pkg->getAppId()){
+                return $this->jsonResponse(
+                    self::HTTP_400_BADREQUEST,
+                    array('error'=>'Invalid package id'));
+            }
 
-			$ret = array();
-			foreach($pkgs as $pkg){
-				$ret[] = $this->makePackageArray($pkg);
-			}
-		}
-		catch(Exception $e){
-			error_log(__METHOD__.'('.__LINE__.'): '.get_class($e).":{$e->getMessage()}");
-			return $this->jsonResponse(
-				self::HTTP_500_INTERNALSERVERERROR,
-				array('error'=>$e->getMessage(),'exception'=>get_class($e)));
-		}
+            # create install token
+            $token_expire = '+1 hours';
 
-		apache_log('app_id',$app->getId());
+            $expire_time = strtotime($token_expire);
+            $mc_expire = $expire_time - time();
 
-		return $this->jsonResponse(self::HTTP_200_OK,$ret);
-	}
+            $tokendata = array(
+                'mail' => $mail,
+                'package_id' => $pkg_id,
+                'expire' => date('Y-m-d H:i:s',$expire_time),
+                );
+            $token = Random::string(32);
+            mfwMemcache::set(self::INSTALL_TOKEN_PREFIX.$token,json_encode($tokendata),$mc_expire);
+
+            apache_log('token',$token);
+            apache_log('token_data',$tokendata);
+
+            # return
+            $red = $this->makePackageArray($pkg);
+            $ret['install_url'] = mfwRequest::makeURL("/package/install?token={$token}");
+
+        }
+        catch(Exception $e){
+            error_log(__METHOD__.'('.__LINE__.'): '.get_class($e).":{$e->getMessage()}");
+            return $this->jsonResponse(
+                self::HTTP_500_INTERNALSERVERERROR,
+                array('error'=>$e->getMessage(),'exception'=>get_class($e)));
+        }
+
+        apache_log('app_id',$app->getId());
+
+        return $this->jsonResponse(self::HTTP_200_OK,$ret);
+    }
 
 }
 
