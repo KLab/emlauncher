409ec88c35abaa7576c77e3099583f6a66c5d926
KeisukeUtsumi <utsumi-k@klab.com>
2016-01-13 19:18:58 +0900

    QR生成Actionsの作成

---
+++ mainmodules/qr/actions/actions.php
@@ -0,0 +1,34 @@
+<?php
+require_once (APP_ROOT.'/libs/QrCode/src/QrCode.php');
+use Endroid\QrCode\QrCode;
+
+class qrActions extends MainActions
+{
+    public function executeCode()
+    {
+        $text = mfwRequest::param('chl', "");
+        $size =(int)mfwRequest::param('size', 150);
+        if ($size > 300) {
+            $size = 300;
+        }
+        $qr_code = new QrCode();
+        $qr_code
+            ->setText($text)
+            ->setSize($size)
+            ->setPadding(10)
+            ->setErrorCorrection('high');
+        return $this->pngResponse(
+            $qr_code->get()
+        );
+
+    }
+
+    private function pngResponse($contents)
+    {
+        $header = array(
+            'Content-type: image/png',
+        );
+        return array($header,$contents);
+    }
+
+}
