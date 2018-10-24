fb97f46d4bbac6fdd2f816bc16f25abb4e4f2b58
inada-s <inada-s@klab.com>
2018-10-24 18:24:09 +0900

    Update entrypoint.sh

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "rewriting hosts..."
+echo "editing hosts..."
 line1=$(cat /etc/hosts | grep -v "localhost" | tail -n 1)
 line2=$(echo $line1 | awk '{print $2}')
 echo "$line1 $line2.localdomain" >> /etc/hosts
 
 cat /etc/hosts
 
-echo "editing sendmail..."
+echo "restarting sendmail..."
 service sendmail restart &
 
 echo "run docker-php-entrypoint"
