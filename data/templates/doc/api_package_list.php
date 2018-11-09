38201fbdbac3daff5b4a04023222afb28483fec2
inada-s <inada-s@klab.com>
2018-11-09 20:10:23 +0900

    support filtering by tags

--- data/templates/doc/api_package_list.php
+++ data/templates/doc/api_package_list.php
@@ -26,6 +26,7 @@
             <li><code>api_key</code> - Required (See the application preferences)</li>
             <li><code>limit</code> - Optional, The limit length of package list. (default = 20, max = 100)</li>
             <li><code>platform</code> - Optional, The platform type string, e.g. "Android" and "iOS".</li>
+            <li><code>tags</code> - Optional, The tag names separated by comma.</li>
           </ul>
         </dd>
       </dl>
