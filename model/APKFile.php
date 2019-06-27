31fdb5ef3bfb68f3e33e6387484713dc1f2c0db9
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2019-06-27 17:37:53 +0900

    remove debug code

--- model/APKFile.php
+++ model/APKFile.php
@@ -5,7 +5,6 @@ class APKFile {
 	public static function getPackageName($apkfile)
 	{
 		$apk = new ApkParser\Parser($apkfile);
-        sleep(20);
 		return $apk->getManifest()->getPackageName();
 	}
 
